745d304da812badc6c552750911e514271124950
M-Ulmann <marek.ulmannek@gmail.com>
2026-09-09 16:08:10 +0200

    Pridane odstraneni konkretniho itemu i celeho seznamu

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,14 @@ import { Hamburger } from "@/components/hamburger";
 import { useState } from "react";
 import clsx from "clsx";
 
+//import { Items } from "@/components/items";
+
 export default function Todo(){
 
   const [isOpenHamburger, setIsOpenHamburger] = useState(true);
   const [isDark, setIsDark] = useState(true);
+  const [itemName, setItemName] = useState("");
+  const [todo, setTodo] = useState<string[]>([]);
 
   return(
     <>
@@ -33,52 +37,79 @@ export default function Todo(){
 
           <div className=" flex flex-col gap-5 justify-center text-2xl sm:flex-row sm:gap-0">
             <input
-              className="flex flex-1 text-2xl pl-5 py-2 rounded-2xl  sm:rounded-l-2xl sm:rounded-r-none focus:outline-cyan-500 focus:outline-2 focus:placeholder:none"
+              className="flex flex-1 text-2xl pl-5 py-2 rounded-2xl  sm:rounded-l-2xl sm:rounded-r-none focus:outline-cyan-500 focus:outline-2 focus:placeholder:text-transparent"
               placeholder="eg. tool" 
               type="text"
               maxLength={40}
+              onChange={(event) => {
+                setItemName(event.target.value);
+              }}
+              value={itemName}
+              onKeyDown={(stisk) =>{
+                if (stisk.key === 'Enter' && itemName.length > 1){
+                  setTodo([...todo, itemName]);
+                  setItemName('');
+                }
+                else if (itemName.length < 1){
+                  console.log("vyber delsi jmeno");
+                }
+              }}
             />
 
             <button className=" cursor-pointer bg-fuchsia-900 text-white px-5 border-2 rounded-2xl border-fuchsia-900 hover:bg-white hover:text-fuchsia-900 active:bg-fuchsia-900 active:text-white transition ease duration-250 sm:rounded-r-2xl sm:rounded-l-none sm:justify-self-center"
+              onClick={() => {
+                if(itemName.length > 1){
+                  setTodo([...todo, itemName]);
+                  setItemName('');
+                }
+              }}
             >
               Submit
             </button>
           </div>
 
-          <div className="flex flex-col gap-6 px-5 text-2xl max-h-50 overflow-x-hidden overflow-y-auto overflow-scroll">
+          <div className="flex flex-col gap-6 p-5 text-2xl max-h-60 overflow-x-hidden overflow-y-auto overflow-scroll ">
 
-            <div className="flex flex-row gap-5 items-center">
+            {todo.map((item, index) => (
+              <div className="flex flex-row gap-5 items-center" key={index}>
 
-              <p className="flex-1">
-                vejce dhbkh jkhkjsdh bhdkjha
-              </p>
-
-              <button
-                className="cursor-pointer p-1 rounded-md  hover:bg-gray-200 hover:outline-2 hover:outline-blue-500 active:opacity-70"
-              >
-                <Image 
-                  src={Edit}
-                  width={40}
-                  alt="Edit image"
-                />
-              </button>
-
-              <button
-                className="cursor-pointer p-1 rounded-md hover:bg-gray-200 hover:outline-2 hover:outline-red-500 active:opacity-70"
-              >
-                <Image 
-                  src={Bin}
-                  width={40}
-                  alt="picture" 
-                />
-              </button>
-
-            </div>
+                <p className="flex-1">
+                  {item}
+                </p>
+          
+                <button
+                  className="cursor-pointer p-1 rounded-md  hover:bg-gray-200 hover:outline-2 hover:outline-blue-500 active:opacity-70"
+                >
+                  <Image 
+                    src={Edit}
+                    width={40}
+                    alt="Edit image"
+                  />
+                </button>
+          
+                <button
+                  className="cursor-pointer p-1 rounded-md hover:bg-gray-200 hover:outline-2 hover:outline-red-500 active:opacity-70"
+                  onClick={() => {
+                    setTodo(deleteItem => deleteItem.filter((_, safeItems) => safeItems !== index))
+                  }}
+                >
+                  <Image 
+                    src={Bin}
+                    width={40}
+                    alt="Bin image" 
+                  />
+                </button>
+          
+              </div>
+            ))}
 
           </div>
 
           <button
             className="cursor-pointer text-red-600 text-3xl self-center px-8 py-3 rounded-4xl hover:bg-red-500 hover:text-white active:bg-red-800 transition ease duration-250"
+            onClick={() => {
+              setTodo([]);
+            }}
           >
             Clear Items
           </button>
